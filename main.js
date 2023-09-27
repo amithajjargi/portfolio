085071dac9d489119a216e8a5db166a6a679cb7c
Amithajjargi123 <139846525+Amithajjargi123@users.noreply.github.com>
2023-09-27 10:26:18 +0530

    l

---
+++ main.js
@@ -0,0 +1,33 @@
+*
+{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'poppins'  , sans-serif;
+
+}
+Body
+{
+    color: #ededed;
+}
+header
+{
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    padding: 20px 10%;
+    background: transparent;
+    display: flex;
+    justify-content: space-between;
+    align-items: Centre;
+    z-index: 100;
+}
+./portfolio/assets/dist/css/bootstrap.min.css {
+    position: relative;
+    font-size: 25px;
+    color: #fff;
+    text-decoration: none;
+    font-weight:
+}
+
